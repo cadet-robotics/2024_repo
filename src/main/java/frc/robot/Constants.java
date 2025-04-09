9dd87ab14ba66e47b1d7cef98f31de3da9237684
cmacdonaldmuckle <macdonaldmucklecolin@gmail.com>
2025-04-09 19:08:07 -0400

    Made branch separate branch to update Ruckus code to 2025. Currently Working

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -44,8 +44,8 @@ public final class Constants
         public static final double SLOW_FACTOR = .5;
         public static final double DEADZONE = 0.13;
         public static final double INTAKE_SPEED = .8; // Was at .5
-        public static final int LAUNCH_1_SPEED = 5000;// for 60% use 3400, was at 4700 for 80%;
-        public static final int LAUNCH_2_SPEED = 5000;// for 75% use 4300;
+        public static final int LAUNCH_1_SPEED = 3400;// for 60% use 3400, was at 4700 for 80%, 100% is 5000;
+        public static final int LAUNCH_2_SPEED = 4300;// for 75% use 4300, 100% is 5000;
         public static final double DELAY_FOR_SHOOT_START = 1.5;
         public static final double DELAY_FOR_SHOOTING = 2;
         public static final int BLINKIN_LED_CONTROLLER_PORT = 9;
